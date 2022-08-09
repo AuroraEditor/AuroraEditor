d7a406b261ced8e52cb7626e9665382c650001b8
Tihan-Nico <tihan-nico@plexus.dev>
2022-08-09 12:19:12 +0200

    Rename `PlaceholderWindowController` to `AboutWindowHostingController`

---
+++ AuroraEditor/Base/About/Controller/AboutWindowHostingController.swift
@@ -0,0 +1,56 @@
+//
+//  AboutWindowHostingController.swift
+//  AuroraEditor
+//
+//  Created by Nanashi Li on 2022/08/09.
+//  Copyright © 2022 Aurora Company. All rights reserved.
+//
+
+import SwiftUI
+
+final class AboutWindowHostingController: NSWindowController {
+    convenience init<T: View>(view: T, size: NSSize) {
+        let hostingController = NSHostingController(rootView: view)
+        // New window holding our SwiftUI view
+        let window = NSWindow(contentViewController: hostingController)
+        self.init(window: window)
+        window.setContentSize(size)
+        window.styleMask.remove(.resizable)
+        window.styleMask.insert(.fullSizeContentView)
+        window.alphaValue = 0.5
+        window.styleMask.remove(.miniaturizable)
+    }
+
+    override func showWindow(_ sender: Any?) {
+        window?.center()
+        window?.alphaValue = 0.0
+
+        super.showWindow(sender)
+
+        window?.animator().alphaValue = 1.0
+
+        // Close the window when the escape key is pressed
+        NSEvent.addLocalMonitorForEvents(matching: .keyDown) { event in
+            guard event.keyCode == 53 else { return event }
+
+            self.closeAnimated()
+
+            return nil
+        }
+
+        window?.collectionBehavior = [.transient, .ignoresCycle]
+        window?.isMovableByWindowBackground = true
+        window?.titlebarAppearsTransparent = true
+        window?.titleVisibility = .hidden
+    }
+
+    func closeAnimated() {
+        NSAnimationContext.beginGrouping()
+        NSAnimationContext.current.duration = 0.4
+        NSAnimationContext.current.completionHandler = {
+            self.close()
+        }
+        window?.animator().alphaValue = 0.0
+        NSAnimationContext.endGrouping()
+    }
+}
--- AuroraEditor/Base/About/UI/AboutView.swift
+++ AuroraEditor/Base/About/UI/AboutView.swift
@@ -113,57 +113,10 @@ public struct AboutView: View {
     }
 
     public func showWindow(width: CGFloat, height: CGFloat) {
-        PlaceholderWindowController(view: self,
+        AboutWindowHostingController(view: self,
                                     size: NSSize(width: width,
                                                  height: height))
         .showWindow(nil)
-    }
-}
-
-final class PlaceholderWindowController: NSWindowController {
-    convenience init<T: View>(view: T, size: NSSize) {
-        let hostingController = NSHostingController(rootView: view)
-        // New window holding our SwiftUI view
-        let window = NSWindow(contentViewController: hostingController)
-        self.init(window: window)
-        window.setContentSize(size)
-        window.styleMask.remove(.resizable)
-        window.styleMask.insert(.fullSizeContentView)
-        window.alphaValue = 0.5
-        window.styleMask.remove(.miniaturizable)
-    }
-
-    override func showWindow(_ sender: Any?) {
-        window?.center()
-        window?.alphaValue = 0.0
-
-        super.showWindow(sender)
-
-        window?.animator().alphaValue = 1.0
-
-        // Close the window when the escape key is pressed
-        NSEvent.addLocalMonitorForEvents(matching: .keyDown) { event in
-            guard event.keyCode == 53 else { return event }
-
-            self.closeAnimated()
-
-            return nil
-        }
-
-        window?.collectionBehavior = [.transient, .ignoresCycle]
-        window?.isMovableByWindowBackground = true
-        window?.titlebarAppearsTransparent = true
-        window?.titleVisibility = .hidden
-    }
-
-    func closeAnimated() {
-        NSAnimationContext.beginGrouping()
-        NSAnimationContext.current.duration = 0.4
-        NSAnimationContext.current.completionHandler = {
-            self.close()
-        }
-        window?.animator().alphaValue = 0.0
-        NSAnimationContext.endGrouping()
     }
 }
 
