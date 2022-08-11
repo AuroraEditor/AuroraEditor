5f73135c49bed162770d97f59f6c49c0d23364ed
Tihan-Nico <tihan-nico@plexus.dev>
2022-08-11 14:04:41 +0200

    Source Control Changes now uses state with the new OutlineView

--- AuroraEditor/Base/NavigatorSidebar/UI/SourceControlNavigator/Changes/ChangesView.swift
+++ AuroraEditor/Base/NavigatorSidebar/UI/SourceControlNavigator/Changes/ChangesView.swift
@@ -7,13 +7,59 @@
 
 import SwiftUI
 
-// TODO: Convert to OutlineView @NanashiLi
 struct ChangesView: View {
 
     @ObservedObject
     var workspace: WorkspaceDocument
 
+    @ObservedObject
+    private var prefs: AppPreferencesModel = .shared
+
     var body: some View {
-        SourceControlView(workspace: workspace)
+        VStack(alignment: .center) {
+            if prefs.sourceControlActive() {
+                switch workspace.workspaceClient?.model?.state {
+                case .success:
+                    if workspace.workspaceClient!.model!.changed.isEmpty {
+                        Text("No Changes")
+                            .font(.system(size: 16))
+                            .foregroundColor(.secondary)
+                    } else {
+                        SourceControlView(workspace: workspace)
+                    }
+                case .loading:
+                    VStack {
+                        Text("Loading Changes")
+                            .font(.system(size: 16))
+                            .foregroundColor(.secondary)
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                case .error:
+                    VStack {
+                        Text("Failed To Find Changes")
+                            .font(.system(size: 16))
+                            .foregroundColor(.secondary)
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                case .none:
+                    VStack {
+                        Text("Failed To Find Changes")
+                            .font(.system(size: 16))
+                            .foregroundColor(.secondary)
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                }
+            } else {
+                VStack {
+                    Text("Source Control Disabled")
+                        .font(.system(size: 16))
+                        .foregroundColor(.secondary)
+
+                    Text("Enable Source Control in settings")
+                        .font(.system(size: 10))
+                }
+            }
+        }
+        .frame(maxHeight: .infinity)
     }
 }
