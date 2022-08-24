5d75766cd150188291311487557c17bbacf4c91d
Ashwin <ashwonixer123@gmail.com>
2022-08-24 19:44:47 -0400

    Fixed swift-lint errors

--- AuroraEditor/Base/WebView/UI/WebWKView.swift
+++ AuroraEditor/Base/WebView/UI/WebWKView.swift
@@ -43,9 +43,10 @@ struct WebWKView: NSViewRepresentable {
     func makeNSView(context: Context) -> NSView {
         let webKitView = WKWebView()
         webKitView.navigationDelegate = context.coordinator
-        
+
         // Configure the webView
-        webKitView.customUserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.0 Safari/605.1.15"
+        webKitView.customUserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) " +
+        "AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.0 Safari/605.1.15"
 
         // load the initial page
         loadPage(webView: webKitView, url: pageURL)
