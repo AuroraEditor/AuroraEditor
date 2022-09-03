d62223fcb57036c81bb9697d21086afae1f93167
Tay Kai Quan <kaitay303@gmail.com>
2022-09-03 16:49:53 +0800

    Migrate QuickOpenView to ActionAwareInput

--- AuroraEditor/Base/QuickOpen/UI/QuickOpenView.swift
+++ AuroraEditor/Base/QuickOpen/UI/QuickOpenView.swift
@@ -23,6 +23,53 @@ public struct QuickOpenView: View {
         self.openFile = openFile
     }
 
+    func onKeyDown(with event: NSEvent) -> Bool {
+        switch event.keyCode {
+        case 125: // down arrow
+            selectOffset(by: 1)
+            return true
+
+        case 126: // up arrow
+            selectOffset(by: -1)
+            return true
+
+        case 36: // enter key
+            if let selectedItem = selectedItem {
+                openFile(selectedItem)
+            }
+            self.onClose()
+            return true
+
+        case 53: // esc key
+            self.onClose()
+            return true
+
+        default:
+            return false
+        }
+    }
+
+    func onQueryChange(text: String) {
+        state.fetchOpenQuickly()
+        if !state.isShowingOpenQuicklyFiles {
+            selectedItem = nil
+        }
+    }
+
+    func selectOffset(by offset: Int) {
+        guard !state.openQuicklyFiles.isEmpty else { return }
+
+        // check if a command has been selected
+        if let selectedItem = selectedItem, let currentIndex = state.openQuicklyFiles.firstIndex(of: selectedItem) {
+            // select current index + offset, wrapping around if theres underflow or overflow.
+            let newIndex = (currentIndex + offset + state.openQuicklyFiles.count) % (state.openQuicklyFiles.count)
+            self.selectedItem = state.openQuicklyFiles[newIndex]
+        } else {
+            // if theres no selected command, just select the first or last item depending on direction
+            selectedItem = state.openQuicklyFiles[ offset < 0 ? state.openQuicklyFiles.count-1 : 0]
+        }
+    }
+
     public var body: some View {
         VStack(spacing: 0.0) {
             VStack {
@@ -33,49 +80,57 @@ public struct QuickOpenView: View {
                         .frame(width: 20, height: 20)
                         .padding(.trailing, 12)
                         .offset(x: 0, y: 1)
-                    TextField("Open Quickly", text: $state.openQuicklyQuery)
-                        .font(.system(size: 20, weight: .light, design: .default))
-                        .textFieldStyle(.plain)
-                        .onReceive(
-                            state.$openQuicklyQuery
-                                .debounce(for: .seconds(0.4), scheduler: DispatchQueue.main)
-                        ) { _ in
-                            state.fetchOpenQuickly()
-                        }
+                    ActionAwareInput(onDown: onKeyDown,
+                                     onTextChange: onQueryChange,
+                                     text: $state.openQuicklyQuery)
                 }
-                    .padding(16)
-                    .foregroundColor(.primary.opacity(0.85))
-                    .background(EffectView(.sidebar, blendingMode: .behindWindow))
+                .padding(16)
+                .foregroundColor(.primary.opacity(0.85))
+                .background(EffectView(.sidebar, blendingMode: .behindWindow))
             }
-            Divider()
-            NavigationView {
-                List(state.openQuicklyFiles, id: \.id) { file in
-                    NavigationLink(tag: file, selection: $selectedItem) {
-                        QuickOpenPreviewView(item: file)
-                    } label: {
-                        QuickOpenItem(baseDirectory: state.fileURL, fileItem: file)
+            .frame(height: 52)
+            if state.isShowingOpenQuicklyFiles {
+                Divider()
+                NavigationView {
+                    ZStack {
+                        List(state.openQuicklyFiles, id: \.id) { file in
+                            NavigationLink(tag: file, selection: $selectedItem) {
+                                QuickOpenPreviewView(item: file)
+                            } label: {
+                                QuickOpenItem(baseDirectory: state.fileURL, fileItem: file)
+                            }
+                            .onTapGesture(count: 2) {
+                                self.openFile(file)
+                                self.onClose()
+                            }
+                            .onTapGesture(count: 1) {
+                                self.selectedItem = file
+                            }
+                        }
+                        .frame(minWidth: 250, maxWidth: 250)
+                        if state.openQuicklyFiles.isEmpty {
+                            EmptyView()
+                        } else {
+                            Text("Select a file to preview")
+                        }
+
+                        Button("") {
+                            if let selectedItem = selectedItem {
+                                self.openFile(selectedItem)
+                                self.onClose()
+                            }
+                        }
+                        .buttonStyle(.borderless)
+                        .keyboardShortcut(.defaultAction)
                     }
-                    .onTapGesture(count: 2) {
-                        self.openFile(file)
-                        self.onClose()
-                    }
-                    .onTapGesture(count: 1) {
-                        self.selectedItem = file
-                    }
-                }
-                .frame(minWidth: 250, maxWidth: 250)
-                if state.openQuicklyFiles.isEmpty {
-                    EmptyView()
-                } else {
-                    Text("Select a file to preview")
                 }
             }
         }
-            .background(EffectView(.sidebar, blendingMode: .behindWindow))
-            .edgesIgnoringSafeArea(.vertical)
-            .frame(minWidth: 600,
-               minHeight: self.state.isShowingOpenQuicklyFiles ? 400 : 28,
-               maxHeight: self.state.isShowingOpenQuicklyFiles ? .infinity : 28)
+        .background(EffectView(.sidebar, blendingMode: .behindWindow))
+        .edgesIgnoringSafeArea(.vertical)
+        .frame(minWidth: 600,
+           minHeight: self.state.isShowingOpenQuicklyFiles ? 400 : 28,
+           maxHeight: self.state.isShowingOpenQuicklyFiles ? .infinity : 28)
     }
 }
 
