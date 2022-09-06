b232fb7e955ca28690d7787e822bf0579d46e4cb
Tay Kai Quan <kaitay303@gmail.com>
2022-09-06 17:25:15 +0800

    Add recent projects to menu bar item

--- AuroraEditor/Base/MenuBarItem/StatusItem.swift
+++ AuroraEditor/Base/MenuBarItem/StatusItem.swift
@@ -18,10 +18,15 @@ extension AppDelegate {
 
         let menu = NSMenu()
 
+        let recentProjectsItem = NSMenuItem(title: "Recent Projects", action: nil, keyEquivalent: "")
+        recentProjectsItem.submenu = NSMenu()
+        updateRecentProjects(in: recentProjectsItem)
+
         menu.items = [
             NSMenuItem(title: "Create a new file", action: #selector(newFile), keyEquivalent: "1"),
             NSMenuItem(title: "Clone an existing project", action: #selector(cloneProject), keyEquivalent: "2"),
             NSMenuItem(title: "Open a project or file", action: #selector(openProject), keyEquivalent: "3"),
+            recentProjectsItem,
             NSMenuItem.separator(),
             NSMenuItem(title: "Preferences", action: #selector(openPreferences), keyEquivalent: ","),
             NSMenuItem(title: "Open Welcome View", action: #selector(openWelcome), keyEquivalent: "e"),
@@ -30,6 +35,46 @@ extension AppDelegate {
             NSMenuItem(title: "Quit", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q")
         ]
         statusItem.menu = menu
+    }
+
+    func updateRecentProjects(in menuItem: NSMenuItem) {
+        let recentProjectPaths = ( UserDefaults.standard.array(forKey: "recentProjectPaths") as? [String] ?? [] )
+            .filter { FileManager.default.fileExists(atPath: $0) }
+
+        menuItem.submenu?.items = recentProjectPaths.map({ name in
+            NSMenuItem(title: name, action: #selector(openFile), keyEquivalent: "")
+        })
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 10) {
+            self.updateRecentProjects(in: menuItem)
+        }
+    }
+
+    @objc
+    func openFile(_ sender: Any?) {
+        guard let sender = sender as? NSMenuItem else { return }
+        let repoPath = sender.title
+        // open the document
+        let repoFileURL = URL(fileURLWithPath: repoPath)
+        Log.info("Opening \(repoFileURL)")
+        AuroraEditorDocumentController.shared.openDocument(
+            withContentsOf: repoFileURL,
+            display: true,
+            completionHandler: { _, _, _ in }
+        )
+        // add to recent projects
+        var recentProjectPaths = (
+            UserDefaults.standard.array(forKey: "recentProjectPaths") as? [String] ?? []
+        ).filter { FileManager.default.fileExists(atPath: $0) }
+        if let urlLocation = recentProjectPaths.firstIndex(of: repoPath) {
+            recentProjectPaths.remove(at: urlLocation)
+        }
+        recentProjectPaths.insert(repoPath, at: 0)
+
+        UserDefaults.standard.set(
+            recentProjectPaths,
+            forKey: "recentProjectPaths"
+        )
     }
 
     @objc
