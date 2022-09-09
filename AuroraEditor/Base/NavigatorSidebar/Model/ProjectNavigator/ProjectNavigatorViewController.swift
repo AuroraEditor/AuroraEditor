07de5c78c808ef1ff78a1d9752e66c85cd3236a8
Tay Kai Quan <kaitay303@gmail.com>
2022-09-09 16:17:10 +0800

    Implement file rearrangement

--- AuroraEditor/Base/NavigatorSidebar/Model/ProjectNavigator/ProjectNavigatorViewController.swift
+++ AuroraEditor/Base/NavigatorSidebar/Model/ProjectNavigator/ProjectNavigatorViewController.swift
@@ -283,36 +283,50 @@ extension ProjectNavigatorViewController: NSOutlineViewDataSource {
     // MARK: Drag and Drop
     func outlineView(_ outlineView: NSOutlineView, pasteboardWriterForItem item: Any) -> NSPasteboardWriting? {
         guard let fileItem = item as? FileItem else {
-            Log.info("Item is not file item")
+            Log.error("Item \(item) is not file item")
             return nil
         }
         let pboarditem = NSPasteboardItem()
-        let success = pboarditem.setString(fileItem.url.path, forType: dragType)
+        pboarditem.setString(fileItem.url.path, forType: dragType)
         return pboarditem
     }
 
     func outlineView(_ outlineView: NSOutlineView, validateDrop info: NSDraggingInfo,
                      proposedItem item: Any?, proposedChildIndex index: Int) -> NSDragOperation {
-        guard let fileItem = item as? FileItem,
+        guard let targetFileItem = item as? FileItem,
               let draggedString = info.draggingPasteboard.string(forType: dragType)
         else { return NSDragOperation(arrayLiteral: [])}
+        let draggedURL = URL(fileURLWithPath: draggedString)
 
         // if the item is being dragged onto an item that it contains, do not move it.
-        // TODO: Fix edge cases like ~/Some not being able to move to ~/Something/ABC
-        if fileItem.isFolder && !fileItem.url.path.hasPrefix(draggedString) {
-            return NSDragOperation.move
-        }
-        return NSDragOperation(arrayLiteral: [])
+
+        // the target must be a folder
+        // the target must not be within the dragged url
+        // the target's url and dragged url's parent must differ
+        guard targetFileItem.isFolder &&
+              !targetFileItem.url.path.hasPrefix(draggedString) &&
+              draggedURL.deletingLastPathComponent() != targetFileItem.url
+        else { return NSDragOperation(arrayLiteral: []) }
+
+        return NSDragOperation.move
     }
 
     func outlineView(_ outlineView: NSOutlineView, acceptDrop info: NSDraggingInfo,
                      item: Any?, childIndex index: Int) -> Bool {
-        guard let fileItem = item as? FileItem,
-              let draggedString = info.draggingPasteboard.string(forType: dragType),
-              fileItem.isFolder && !fileItem.url.path.hasPrefix(draggedString)
+        // just reuse the validation function to check if the drop should happen
+        guard self.outlineView(outlineView, validateDrop: info,
+                               proposedItem: item, proposedChildIndex: index) == .move,
+              let targetFileItem = item as? FileItem,
+              let draggedString = info.draggingPasteboard.string(forType: dragType)
         else { return false }
         let draggedURL = URL(fileURLWithPath: draggedString)
-        Log.info("Drop: \(fileItem.url) onto \(draggedURL)")
+        do {
+            let fileName = draggedURL.lastPathComponent
+            try FileManager.default.moveItem(at: draggedURL, to: targetFileItem.url.appendingPathComponent(fileName))
+        } catch {
+            Log.error("Moving item \(draggedURL) to \(targetFileItem.url) failed: \(error)")
+            return false
+        }
         return true
     }
 }
