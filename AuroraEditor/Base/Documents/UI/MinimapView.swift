d4a088f358f9d116e417454a1ccef7e83cd1e56c
Tay Kai Quan <kaitay303@gmail.com>
2022-09-16 21:48:16 +0800

    Add current line highlight

---
+++ AuroraEditor/Base/Documents/UI/MinimapView.swift
@@ -0,0 +1,37 @@
+//
+//  MinimapView.swift
+//  AuroraEditor
+//
+//  Created by TAY KAI QUAN on 16/9/22.
+//  Copyright © 2022 Aurora Company. All rights reserved.
+//
+
+import SwiftUI
+
+struct MinimapView: View {
+    @Binding var attributedTextItems: [AttributedStringItem]
+
+    @ObservedObject
+    private var sharedObjects: SharedObjects = .shared
+
+    var body: some View {
+        ZStack(alignment: .topLeading) {
+            Color.black.opacity(0.2)
+                Color.gray.opacity(0.5)
+                    .frame(height: minimapLineHeight)
+                    .offset(y: CGFloat(sharedObjects.caretPos.line) * minimapLineHeight + (minimapLineHeight * 0.75))
+            ForEach(attributedTextItems) { textItem in
+                if let color = textItem.attributes[.foregroundColor] as? NSColor {
+                    Color(nsColor: color)
+                        .frame(width: CGFloat(textItem.text.count) * minimapMultiplier,
+                               height: minimapLineHeight)
+                        .offset(x: CGFloat(textItem.charactersFromStart) * minimapMultiplier,
+                                y: CGFloat(textItem.lineNumber) * (minimapLineHeight * 1.5))
+                }
+            }
+        }
+    }
+
+    let minimapMultiplier: CGFloat = 0.8
+    let minimapLineHeight: CGFloat = 2
+}
--- AuroraEditor/Base/Documents/UI/WorkspaceCodeFileView.swift
+++ AuroraEditor/Base/Documents/UI/WorkspaceCodeFileView.swift
@@ -240,25 +240,8 @@ public struct AECodeView: View {
             }
 
             // Minimap
-            ZStack(alignment: .topLeading) {
-                Color.black.opacity(0.2)
-//                Color.gray.opacity(0.5)
-//                    .frame(height: minimapLineHeight)
-//                    .offset(y: CGFloat(sharedObjects.caretPos.line) * minimapLineHeight)
-                ForEach(attributedTextItems) { textItem in
-                    if let color = textItem.attributes[.foregroundColor] as? NSColor {
-                        Color(nsColor: color)
-                            .frame(width: CGFloat(textItem.text.count) * minimapMultiplier,
-                                   height: minimapLineHeight)
-                            .offset(x: CGFloat(textItem.charactersFromStart) * minimapMultiplier,
-                                    y: CGFloat(textItem.lineNumber) * (minimapLineHeight * 1.5))
-                    }
-                }
-            }
+            MinimapView(attributedTextItems: $attributedTextItems)
             .frame(width: 150)
         }
     }
-
-    let minimapMultiplier: CGFloat = 0.8
-    let minimapLineHeight: CGFloat = 2
 }
