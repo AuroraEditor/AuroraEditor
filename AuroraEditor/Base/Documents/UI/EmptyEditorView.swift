d73e1d238426bed7437458d818a71a9a2b478dd0
Nanashi Li <nanashili@plexus.dev>
2022-09-24 19:25:12 +0200

    Fix editor background color

---
+++ AuroraEditor/Base/Documents/UI/EmptyEditorView.swift
@@ -0,0 +1,41 @@
+//
+//  EmptyEditorView.swift
+//  AuroraEditor
+//
+//  Created by Nanashi Li on 2022/09/24.
+//  Copyright © 2022 Aurora Company. All rights reserved.
+//
+
+import SwiftUI
+
+struct EmptyEditorView: View {
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .foregroundStyle(.bar)
+            Text("No Editor")
+                .font(.system(size: 17))
+                .foregroundColor(.secondary)
+                .frame(minHeight: 0)
+                .clipped()
+                .overlay {
+                    Button(
+                        action: {
+                            NSApplication.shared.keyWindow?.close()
+                        },
+                        label: { EmptyView() }
+                    )
+                    .frame(width: 0, height: 0)
+                    .padding(0)
+                    .opacity(0)
+                    .keyboardShortcut("w", modifiers: [.command])
+                }
+        }
+    }
+}
+
+struct EmptyEditorView_Previews: PreviewProvider {
+    static var previews: some View {
+        EmptyEditorView()
+    }
+}
