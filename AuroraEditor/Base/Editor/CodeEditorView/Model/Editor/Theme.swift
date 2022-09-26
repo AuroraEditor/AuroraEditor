d12d8e6fe62135f7ba703e846b3f71212beaa096
Tay Kai Quan <kaitay303@gmail.com>
2022-09-26 21:08:51 +0800

    include `EditorTheme` in `Theme`

--- AuroraEditor/Base/Editor/CodeEditorView/Model/Editor/Theme.swift
+++ AuroraEditor/Base/Editor/CodeEditorView/Model/Editor/Theme.swift
@@ -7,84 +7,91 @@
 //  This module defines code highlight themes.
 
 import SwiftUI
+import EditorCore
 
 /// A code highlighting theme. Different syntactic elements are purely distinguished by colour.
 ///
 public struct Theme: Identifiable {
-  public let id = UUID()
+    public let id = UUID()
 
-  /// The name of the font to use.
-  public var fontName: String
+    /// The name of the font to use.
+    public var fontName: String
 
-  /// The point size of the font to use.
-  ///
-  public var fontSize: CGFloat
+    /// The point size of the font to use.
+    ///
+    public var fontSize: CGFloat
 
-  /// The default foreground text colour.
-  public var textColour: OSColor
+    /// The default foreground text colour.
+    public var textColour: OSColor
 
-  /// The colour for (all kinds of) comments.
-  public var commentColour: OSColor
+    /// The colour for (all kinds of) comments.
+    public var commentColour: OSColor
 
-  /// The colour for string literals.
-  public var stringColour: OSColor
+    /// The colour for string literals.
+    public var stringColour: OSColor
 
-  /// The colour for character literals.
-  public var characterColour: OSColor
+    /// The colour for character literals.
+    public var characterColour: OSColor
 
-  /// The colour for number literals.
-  public var numberColour: OSColor
+    /// The colour for number literals.
+    public var numberColour: OSColor
 
-  /// The colour for identifiers.
-  public var identifierColour: OSColor
+    /// The colour for identifiers.
+    public var identifierColour: OSColor
 
-  /// The colour for keywords.
-  public var keywordColour: OSColor
+    /// The colour for keywords.
+    public var keywordColour: OSColor
 
-  /// The background colour.
-  public var backgroundColour: OSColor
+    /// The background colour.
+    public var backgroundColour: OSColor
 
-  /// The colour of the current line highlight.
-  public var currentLineColour: OSColor
+    /// The colour of the current line highlight.
+    public var currentLineColour: OSColor
 
-  /// The colour to use for the selection highlight.
-  public var selectionColour: OSColor
+    /// The colour to use for the selection highlight.
+    public var selectionColour: OSColor
 
-  /// The cursor colour.
-  public var cursorColour: OSColor
+    /// The cursor colour.
+    public var cursorColour: OSColor
 
-  /// The colour to use if invisibles are drawn.
-  public var invisiblesColour: OSColor
+    /// The colour to use if invisibles are drawn.
+    public var invisiblesColour: OSColor
 
-  public init(fontName: String,
-              fontSize: CGFloat,
-              textColour: OSColor,
-              commentColour: OSColor,
-              stringColour: OSColor,
-              characterColour: OSColor,
-              numberColour: OSColor,
-              identifierColour: OSColor,
-              keywordColour: OSColor,
-              backgroundColour: OSColor,
-              currentLineColour: OSColor,
-              selectionColour: OSColor,
-              cursorColour: OSColor,
-              invisiblesColour: OSColor) {
-    self.fontName = fontName
-    self.fontSize = fontSize
-    self.textColour = textColour
-    self.commentColour = commentColour
-    self.stringColour = stringColour
-    self.characterColour = characterColour
-    self.numberColour = numberColour
-    self.identifierColour = identifierColour
-    self.keywordColour = keywordColour
-    self.backgroundColour = backgroundColour
-    self.currentLineColour = currentLineColour
-    self.selectionColour = selectionColour
-    self.cursorColour = cursorColour
-    self.invisiblesColour = invisiblesColour
-  }
+    /// The `EditorTheme` to use in highlighting
+    public var editorTheme: EditorTheme
+
+    public init(fontName: String,
+                fontSize: CGFloat,
+                textColour: OSColor,
+                commentColour: OSColor,
+                stringColour: OSColor,
+                characterColour: OSColor,
+                numberColour: OSColor,
+                identifierColour: OSColor,
+                keywordColour: OSColor,
+                backgroundColour: OSColor,
+                currentLineColour: OSColor,
+                selectionColour: OSColor,
+                cursorColour: OSColor,
+                invisiblesColour: OSColor,
+                editorTheme: EditorTheme? = nil
+    ) {
+        self.fontName = fontName
+        self.fontSize = fontSize
+        self.textColour = textColour
+        self.commentColour = commentColour
+        self.stringColour = stringColour
+        self.characterColour = characterColour
+        self.numberColour = numberColour
+        self.identifierColour = identifierColour
+        self.keywordColour = keywordColour
+        self.backgroundColour = backgroundColour
+        self.currentLineColour = currentLineColour
+        self.selectionColour = selectionColour
+        self.cursorColour = cursorColour
+        self.invisiblesColour = invisiblesColour
+        self.editorTheme = editorTheme ?? exampleTheme
+    }
 }
 
 /// A theme catalog indexing themes by name
@@ -129,30 +136,31 @@ extension Theme {
 
 extension Theme {
 
-  /// Font object on the basis of the font name and size of the theme.
-  var font: OSFont {
-    if fontName.hasPrefix("SFMono") {
+    /// Font object on the basis of the font name and size of the theme.
+    var font: OSFont {
+        if fontName.hasPrefix("SFMono") {
 
-      let weightString = fontName.dropFirst("SFMono".count)
-      let weight: OSFont.Weight
-      switch weightString {
-      case "UltraLight": weight = .ultraLight
-      case "Thin":       weight = .thin
-      case "Light":      weight = .light
-      case "Regular":    weight = .regular
-      case "Medium":     weight = .medium
-      case "Semibold":   weight = .semibold
-      case "Bold":       weight = .bold
-      case "Heavy":      weight = .heavy
-      case "Black":      weight = .black
-      default:           weight = .regular
-      }
-      return OSFont.monospacedSystemFont(ofSize: fontSize, weight: weight)
+            let weightString = fontName.dropFirst("SFMono".count)
+            let weight: OSFont.Weight
+            switch weightString {
+            case "UltraLight": weight = .ultraLight
+            case "Thin":       weight = .thin
+            case "Light":      weight = .light
+            case "Regular":    weight = .regular
+            case "Medium":     weight = .medium
+            case "Semibold":   weight = .semibold
+            case "Bold":       weight = .bold
+            case "Heavy":      weight = .heavy
+            case "Black":      weight = .black
+            default:           weight = .regular
+            }
+            return OSFont.monospacedSystemFont(ofSize: fontSize, weight: weight)
 
-    } else {
+        } else {
 
-      return OSFont(name: fontName, size: fontSize) ?? OSFont.monospacedSystemFont(ofSize: fontSize, weight: .regular)
+            return OSFont(name: fontName, size: fontSize) ?? OSFont.monospacedSystemFont(ofSize: fontSize,
+                                                                                         weight: .regular)
 
+        }
     }
-  }
 }
