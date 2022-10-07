dbcf51fee3a754746847190135b4c70f36be0540
Wesley de Groot <email@wesleydegroot.nl>
2022-10-07 11:09:38 +0200

    Updated package

--- Oniguruma/Package.swift
+++ Oniguruma/Package.swift
@@ -1,11 +1,23 @@
-// swift-tools-version:5.3
+// swift-tools-version: 5.7
+
 import PackageDescription
 
 let package = Package(
     name: "SwiftOniguruma",
+    platforms: [.macOS(.v12)],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "SwiftOniguruma",
+            targets: ["SwiftOniguruma"])
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
     targets: [
         // .systemLibrary(name: "Oniguruma", pkgConfig: "Oniguruma"),
-        .systemLibrary(name: "Oniguruma", path: "Sources/Oniguruma"),
-        .target(name: "SwiftOniguruma", dependencies: ["Oniguruma"])
+        .systemLibrary(name: "oniguruma", path: "Sources/oniguruma"),
+        .target(name: "SwiftOniguruma", dependencies: ["oniguruma"])
     ]
 )
