ef298681f98eafa23d5e9ac2ddf99f9594163476
Wesley de Groot <email@wesleydegroot.nl>
2022-11-11 21:17:24 +0100

    Docs: GitlabAccount.swift

--- AuroraEditor/Base/Version Control/Model/Accounts/Gitlab/GitlabAccount.swift
+++ AuroraEditor/Base/Version Control/Model/Accounts/Gitlab/GitlabAccount.swift
@@ -7,13 +7,18 @@
 
 import Foundation
 
-// TODO: DOCS (Nanashi Li)
+/// Gitlab Base URL
 public let gitlabBaseURL = "https://gitlab.com/api/v4/"
+
+/// Gitlab Web URL
 public let gitlabWebURL = "https://gitlab.com/"
 
 public struct GitlabAccount {
+    /// GIT Configuration
     public let configuration: GitConfiguration
 
+    /// Initialize Gitlab cccount
+    /// - Parameter config: GIT Configuration
     public init(_ config: GitConfiguration = GitlabTokenConfiguration()) {
         configuration = config
     }
