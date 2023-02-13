92628dbf42bd9596b3ee72c6ceb990f9ce1477fc
Wesley de Groot <email@wesleydegroot.nl>
2023-02-13 06:28:04 +0100

    Fixed some swiftlint errors (#424)
    # Description
    
    Fixed some swiftlint errors
    
    # Related Issue
    
    <!--- REQUIRED: Tag all related issues (e.g. * #23) -->
    * N/A
    
    # Checklist
    
    <!--- Add things that are not yet implemented above -->
    - [x] My changes generate no new warnings
    - [x] My code builds and runs on my machine
    - [x] I documented my code
    - [/] I added localization
    - [/] I added tests (and they pass)
    
    # Screenshots
    
    <!--- REQUIRED: if issue is UI related -->
    
    N/A
    
    <!--- IMPORTANT: Fill out all required fields. Otherwise we might close
    this PR temporarily -->

--- AuroraEditor/Base/Documents/Model/AuroraEditorWindow/AuroraDataStorage.swift
+++ AuroraEditor/Base/Documents/Model/AuroraEditorWindow/AuroraDataStorage.swift
@@ -16,48 +16,71 @@ class AuroraDataStorage: ObservableObject {
         self.windowController = windowController
     }
 
-    // NOTE: For the sake of clean code, I disable opening_brace so that the didSet commands
-    // are visually apart from the variable.
-
-    // swiftlint:disable:next swiftlint_file_disabling
-    // swiftlint:disable opening_brace
-
     // Navigator settings
     @Published
-    var sortFoldersOnTop: Bool = true                               { didSet { update() } }
+    var sortFoldersOnTop: Bool = true {
+        didSet { update() }
+    }
 
     // Open sheets in the editor
     @Published
-    var showStashChangesSheet: Bool = false                         { didSet { update() } }
+    var showStashChangesSheet: Bool = false {
+        didSet { update() }
+    }
     @Published
-    var showRenameBranchSheet: Bool = false                         { didSet { update() } }
+    var showRenameBranchSheet: Bool = false {
+        didSet { update() }
+    }
     @Published
-    var showAddRemoteView: Bool = false                             { didSet { update() } }
+    var showAddRemoteView: Bool = false {
+        didSet { update() }
+    }
     @Published
-    var showBranchCreationSheet: Bool = false                       { didSet { update() } }
+    var showBranchCreationSheet: Bool = false {
+        didSet { update() }
+    }
     @Published
-    var showTagCreationSheet: Bool = false                          { didSet { update() } }
+    var showTagCreationSheet: Bool = false {
+        didSet { update() }
+    }
 
     // Git data for the editor
     @Published
-    var currentlySelectedBranch: String = ""                        { didSet { update() } }
+    var currentlySelectedBranch: String = "" {
+        didSet { update() }
+    }
     @Published
-    var branchRevision: String = ""                                 { didSet { update() } }
+    var branchRevision: String = "" {
+        didSet { update() }
+    }
     @Published
-    var commitHash: String = ""                                     { didSet { update() } }
+    var commitHash: String = "" {
+        didSet { update() }
+    }
     @Published
-    var branchRevisionDescription: String = ""                      { didSet { update() } }
+    var branchRevisionDescription: String = "" {
+        didSet { update() }
+    }
 
     // Editor information
     @Published
-    public var caretPos: CursorLocation = .init(line: 0, column: 0) { didSet { update() } }
+    public var caretPos: CursorLocation = .init(line: 0, column: 0) {
+        didSet { update() }
+    }
     @Published
-    public var bracketCount: BracketCount = .init(roundBracketCount: 0, curlyBracketCount: 0,
-                                                  squareBracketCount: 0, bracketHistory: [])
-                                                                    { didSet { update() } }
+    public var bracketCount: BracketCount = .init(
+        roundBracketCount: 0,
+        curlyBracketCount: 0,
+        squareBracketCount: 0,
+        bracketHistory: []
+    ) {
+        didSet { update() }
+
+    }
     @Published
-    var currentToken: Token?                                        { didSet { update() } }
-    // swiftlint:enable opening_brace
+    var currentToken: Token? {
+        didSet { update() }
+    }
 
     /// Function that updates the ``AuroraEditorWindowController`` and the ``WorkspaceDocument``.
     /// Views may reference this class via either of them, and therefore both need to be updated in order
