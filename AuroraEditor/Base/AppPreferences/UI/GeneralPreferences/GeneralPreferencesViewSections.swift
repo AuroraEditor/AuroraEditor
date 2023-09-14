fe2d6b6ea9024b1ca0a7701e4a08f1e21b251eb4
Wesley de Groot <email@wesleydegroot.nl>
2023-09-14 20:57:37 +0200

    Small fix for settings

--- AuroraEditor/Base/AppPreferences/UI/GeneralPreferences/GeneralPreferencesViewSections.swift
+++ AuroraEditor/Base/AppPreferences/UI/GeneralPreferences/GeneralPreferencesViewSections.swift
@@ -163,7 +163,7 @@ extension GeneralPreferencesView {
                 Text("settings.general.open.welcome")
                     .tag(AppPreferences.ReopenBehavior.welcome)
                 Divider()
-                Text("settings.general.open.pane")
+                Text("settings.general.open.panel")
                     .tag(AppPreferences.ReopenBehavior.openPanel)
                 Text("settings.general.open.document")
                     .tag(AppPreferences.ReopenBehavior.newDocument)
