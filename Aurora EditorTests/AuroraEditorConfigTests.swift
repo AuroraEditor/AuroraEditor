92f4f730141db17371a14703237f1ad3d34fe548
Wesley de Groot <email@wesleydegroot.nl>
2024-02-10 14:47:56 +0100

    Find `.editorconfig` file

---
+++ Aurora EditorTests/AuroraEditorConfigTests.swift
@@ -0,0 +1,18 @@
+//
+//  AuroraEditorConfigTests.swift
+//  Aurora Editor Tests
+//
+//  Created by Wesley de Groot on 10/02/2024.
+//  Copyright © 2024 Aurora Company. All rights reserved.
+//
+
+import XCTest
+@testable import AuroraEditor
+
+final class AuroraEditorConfigTests: XCTestCase {
+    func testEditorConfig() throws {
+        // TODO: Create test
+        // get project path.
+        AuroraEditorConfig(fromPath: #file)
+    }
+}
---
+++ AuroraEditor/Utils/AuroraEditorConfig.swift
@@ -0,0 +1,42 @@
+//
+//  AuroraEditorConfig.swift
+//  Aurora Editor
+//
+//  Created by Wesley de Groot on 10/02/2024.
+//  Copyright © 2024 Aurora Company. All rights reserved.
+//
+
+import Foundation
+
+class AuroraEditorConfig {
+    init(fromPath: String) {
+        // try to load from this directory, otherwise go to the parent
+        if let configFile = findEditorConfig(fromPath: fromPath),
+           let configData = FileManager.default.contents(atPath: configFile),
+           let configINI = String(data: configData, encoding: .utf8) {
+            let parsed = AuroraINIParser(ini: configINI).parse()
+            Log.info("INI=\(configFile)")
+            Log.info(parsed)
+        }
+
+    }
+
+    private func findEditorConfig(fromPath: String) -> String? {
+        let components = fromPath.components(separatedBy: "/")
+        for number in stride(from: 0, to: components.count, by: 1) {
+            var components = fromPath.components(separatedBy: "/")
+            for _ in stride(from: 0, to: number, by: 1) {
+                components.removeLast()
+            }
+
+            var temporaryPath = components.joined(separator: "/")
+            temporaryPath += "/.editorconfig"
+
+            if FileManager.default.fileExists(atPath: temporaryPath) {
+                return temporaryPath
+            }
+        }
+
+        return nil
+    }
+}
