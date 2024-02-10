c7d79a222a060c389b5d2a28ce8026647b6277d2
Wesley de Groot <email@wesleydegroot.nl>
2024-02-10 15:58:12 +0100

    Add documentation

--- AuroraEditor/Utils/AuroraEditorConfig.swift
+++ AuroraEditor/Utils/AuroraEditorConfig.swift
@@ -9,21 +9,87 @@
 import Foundation
 
 class AuroraEditorConfig {
+    /// .editorconfig keys
     enum EditorConfigKeys: String {
+        /// Indentation Size (in single-spaced characters)
         case indent_style
         // swiftlint:disable:previous identifier_name
+
+        /// Indentation Size (in single-spaced characters)
         case indent_size
         // swiftlint:disable:previous identifier_name
+
+        /// Width of a single tabstop character
         case tab_width
         // swiftlint:disable:previous identifier_name
+
+        /// Line ending file format (Unix, DOS, Mac)
         case end_of_line
         // swiftlint:disable:previous identifier_name
+
+        // swiftlint:disable:next line_length
+        /// File character encoding ([See Character Set Support](https://github.com/editorconfig/editorconfig/wiki/Character-Set-Support).)
         case charset
+
+        /// Denotes whether whitespace is removed from the end of lines
         case trim_trailing_whitespace
         // swiftlint:disable:previous identifier_name
+
+        /// Denotes whether file should end with a newline
         case insert_final_newline
         // swiftlint:disable:previous identifier_name
+
+        /// Forces hard line wrapping after the amount of characters specified.
+        /// off to turn off this feature (use the editor settings).
         case max_line_length
+        // swiftlint:disable:previous identifier_name
+
+        // EXPERIMENTAL/NON-DEFAULT KEYS
+        /// Denotes preferred quoting style for string literals (for languages that support multiple quote styles)
+        case quote_type
+        // swiftlint:disable:previous identifier_name
+
+        /// Denotes the include paths of header files for some languages.
+        /// Could be used by syntax checkers and compilers inside editors.
+        case c_include_path
+        // swiftlint:disable:previous identifier_name
+
+        /// Denotes the `CLASSPATH` used by the Java source files.
+        /// Could be used by some Java syntax checkers and compilers inside editors.
+        case java_class_path
+        // swiftlint:disable:previous identifier_name
+
+        /// Denotes whether the left part of the curly bracket should be on the next line or not
+        case curly_bracket_next_line
+        // swiftlint:disable:previous identifier_name
+
+        /// Denotes whether spaces should be present around arithmetic and boolean operators
+        case spaces_around_operatorsm
+        // swiftlint:disable:previous identifier_name
+
+        /// Denotes how spaces should be around brackets and parentheses:
+        /// no space, only inside the brackets, only outside the brackets, or at the both side of brackets
+        case spaces_around_brackets
+        // swiftlint:disable:previous identifier_name
+
+        /// Denotes the [style for using curly](https://en.wikipedia.org/wiki/Indent_style)
+        /// braces in code blocks
+        case indent_brace_style
+        // swiftlint:disable:previous identifier_name
+
+        /// Denotes the number of imports required before multiple imports are
+        /// automatically collapsed to a wildcard (or a wildcard is automatically expanded to explicit imports)
+        case wildcard_import_limit
+        // swiftlint:disable:previous identifier_name
+
+        /// Denotes the continuation indent size. Useful to distinguish code blocks versus continuation lines
+        case continuation_indent_size
+        // swiftlint:disable:previous identifier_name
+
+        /// Denotes the block_comment or line_comment character to mark a block or each line as comment.
+        /// Some languages require # prepended on each line, others // or ;.
+        /// Some have specific block start markers and end markers such as /* and */ and <!-- and --!>
+        case block_comment, line_comment, block_comment_start, block_comment_end
         // swiftlint:disable:previous identifier_name
     }
 
@@ -38,7 +104,7 @@ class AuroraEditorConfig {
             "charset": "utf8",
             "trim_trailing_whitespace": "true",
             "insert_final_newline": "true",
-            "max_line_length": "10000"
+            "max_line_length": "off"
         ]
     ]
 
