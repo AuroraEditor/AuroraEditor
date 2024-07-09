a900b49a87f16d5ac588e730c9cc3a396fe5e9db
Wesley de Groot <email@wesleydegroot.nl>
2024-07-09 20:51:40 +0200

    // periphery:ignore documentation classes

--- AuroraEditor/Core/ExtensionSystem/ExtensionDocumentation/ExtensionDoc_fromAE.swift
+++ AuroraEditor/Core/ExtensionSystem/ExtensionDocumentation/ExtensionDoc_fromAE.swift
@@ -11,6 +11,7 @@ import AEExtensionKit
 
 /// This class does not execute any code, this is made to generate documentation for the extension system
 /// This are items which an extension can expect from AuroraEditor.
+// periphery:ignore
 public protocol ExtensionDocumentationFromAuroraEditor {
 
     /// Did open file
--- AuroraEditor/Core/ExtensionSystem/ExtensionDocumentation/ExtensionDoc_toAE.swift
+++ AuroraEditor/Core/ExtensionSystem/ExtensionDocumentation/ExtensionDoc_toAE.swift
@@ -12,6 +12,7 @@ import AEExtensionKit
 
 /// This class does not execute any code, this is made to generate documentation for the extension system
 /// This are items which an extension can expect from AuroraEditor.
+// periphery:ignore
 public protocol ExtensionDocumentationToAuroraEditor {
 
     /// Open settings
